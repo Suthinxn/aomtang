d1002919eae4c772a3b1cd1696d09995713d6ec4
Suthinan Rongphon <suthinan.rpn@gmail.com>
2024-02-08 15:57:08 +0700

    add db

--- aomtang/main.py
+++ aomtang/main.py
@@ -1,9 +1,24 @@
 from flask import Flask, redirect, url_for, render_template, request , session, flash
 from datetime import timedelta
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.secret_key = "password"
 app.permanent_session_lifetime = timedelta(minutes=5)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://user.sqlite3'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+class users(db.ModeL):
+    _id = db.Column("id", db.Integer, primary_key=True)
+    name = db.Column("name", db.String(100))
+    email = db.Column("email", db.String(100))
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
 
 @app.route("/")
 def hello_world():
@@ -33,6 +48,7 @@ def user():
         if request.method == "POST":
             email = request.form["email"]
             session["email"] = email
+            flash("Email was save!")
         else:
             if "email" in session:
                 email = session["email"]
@@ -51,4 +67,5 @@ def logout():
     return redirect(url_for("login"))
 
 if __name__ == "__main__":
+    db.create_all()
     app.run(debug=True)
