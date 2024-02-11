188ee549381d4858ba99d475d6fc567bf951064f
Suthinan Rongphon <suthinan.rpn@gmail.com>
2024-02-11 19:09:52 +0700

    feat:auth

--- aomtang/auth.py
+++ aomtang/auth.py
@@ -1,7 +1,23 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, url_for, request, redirect
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, logout_user, login_required
+from .models import User
+from . import db
 
 auth = Blueprint('auth', __name__)
 
-auth.route('/signup')
-def signup():
-    return 
+@auth.route('/register')
+def register():
+    return render_template("/register_page/register.html")
+
+
+@auth.route("/login")
+def login():
+    return render_template("/login_page/login.html")
+
+
+
+@auth.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("login"))
